68bc9fd806ecb95cd82b0c82b967affa58605360
huatahhhhh <>
2022-11-14 08:30:00 +0000

    Environment setup

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,23 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic_fork = fs.readFileSync(".fork.secret").toString().trim();
+const mnemonic_mumbai = fs.readFileSync(".mumbai.secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
+      port: 8546,
+      network_id: "*", // Match any network id
+      gas: 5000000
+    },
+    mumbai_fork: {
+      provider: () => new HDWalletProvider(mnemonic_fork, `http://127.0.0.1:9393`),
+      network_id: "80001", // Match any network id
+      gas: 5000000
+    },
+    mumbai_testnet: {
+      provider: () => new HDWalletProvider(mnemonic_mumbai, `https://polygon-mumbai.infura.io/v3/a097978523884dd59b3bdbc9781f8de9`),
       network_id: "*", // Match any network id
       gas: 5000000
     }
