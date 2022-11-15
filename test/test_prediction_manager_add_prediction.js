2d28e7549f1592ee0c086e6dd167848d15b584c8
huatahhhhh <>
2022-11-15 15:46:40 +0000

    Checkpoint: modified add_prediction test to be relative to blockchain time

--- test/test_prediction_manager_add_prediction.js
+++ test/test_prediction_manager_add_prediction.js
@@ -1,4 +1,5 @@
-const { expectRevert } = require("@openzeppelin/test-helpers");
+const BN = require('bn.js');
+const { time, expectRevert } = require("@openzeppelin/test-helpers");
 const {ZERO_ADDRESS} = require("@openzeppelin/test-helpers/src/constants");
 const PredictionManagerContract = artifacts.require("PredictionManager");
 const PriceFeedManagerContract = artifacts.require("TestPriceFeedManager");
@@ -6,17 +7,11 @@ const PriceFeedManagerContract = artifacts.require("TestPriceFeedManager");
 const CHAINLINK_BTC_FEED="0x007A22900a3B98143368Bd5906f8E17e9867581b";
 const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 const TOL = 60*60*24;
+const TOL_BN = time.duration.days(1);
 
 //Result as of Block 29151538
 const BTC_PRICE = 1648680000000;
 const TIMESTAMP = 1668445371;
-
-function getUtcSeconds(){
-  const now = new Date(); 
-  const utcMilllisecondsSinceEpoch = now.getTime() + (now.getTimezoneOffset() * 60 * 1000);
-  const utcSecondsSinceEpoch = Math.round(utcMilllisecondsSinceEpoch / 1000);
-  return utcSecondsSinceEpoch;
-}
 
 contract('PredictionManager', ([owner, other1, other2]) => {
   before( async () => {
@@ -43,6 +38,7 @@ contract('PredictionManager', ([owner, other1, other2]) => {
       { from: this.owner }
     );
     await this.priceFeedContract.updateTimeStamp();
+    this.now = await time.latest();
   });
   describe("When non-owner creates a prediction", async () => {
     it("prediction is not saved", async () => {
@@ -50,7 +46,7 @@ contract('PredictionManager', ([owner, other1, other2]) => {
       await expectRevert(
         this.contract.createPrediction(
           this.other1,
-          getUtcSeconds(),
+          this.now,
           "BTC",
           true, // LONG
           60*60*24*90, // 3 months
@@ -68,7 +64,7 @@ contract('PredictionManager', ([owner, other1, other2]) => {
       await expectRevert(
         this.contract.createPrediction(
           this.other1,
-          getUtcSeconds(),
+          this.now,
           "BTC",
           true, // LONG
           60*60*24*90, // 3 months
@@ -88,7 +84,7 @@ contract('PredictionManager', ([owner, other1, other2]) => {
       await expectRevert(
         this.contract.createPrediction(
           this.other1,
-          getUtcSeconds(),
+          this.now,
           "XYY",
           true, // LONG
           60*60*24*90, // 3 months
@@ -107,7 +103,7 @@ contract('PredictionManager', ([owner, other1, other2]) => {
       await expectRevert(
         this.contract.createPrediction(
           this.other1,
-          getUtcSeconds(),
+          this.now,
           "XYV",
           true, // LONG
           60*60*24*90, // 3 months
@@ -123,11 +119,14 @@ contract('PredictionManager', ([owner, other1, other2]) => {
  
   describe("When owner creates a prediction and prediction timestamp is too early", async () => {
     it("prediction is not saved", async () => {
+      //here/
+      //
+      var earlier = this.now.sub(TOL_BN.mul(new BN(2)));
       await this.contract.addUser(this.other1);
       await expectRevert(
         this.contract.createPrediction(
           this.other1,
-          getUtcSeconds() - (1.5*TOL),
+          earlier,
           "BTC",
           true, // LONG
           60*60*24*90, // 3 months
@@ -142,11 +141,13 @@ contract('PredictionManager', ([owner, other1, other2]) => {
   });
   describe("When owner creates a prediction and prediction timestamp is too late", async () => {
     it("prediction is not saved", async () => {
+
+      var later = this.now.add(TOL_BN.mul(new BN(2)));
       await this.contract.addUser(this.other1);
       await expectRevert(
         this.contract.createPrediction(
           this.other1,
-          getUtcSeconds() + (1.5*TOL),
+          later,
           "BTC",
           true, // LONG
           60*60*24*90, // 3 months
@@ -165,7 +166,7 @@ contract('PredictionManager', ([owner, other1, other2]) => {
       await expectRevert(
         this.contract.createPrediction(
           this.other1,
-          getUtcSeconds(),
+          this.now,
           "BTC",
           true, // LONG
           TOL, // too short
@@ -178,15 +179,16 @@ contract('PredictionManager', ([owner, other1, other2]) => {
       assert.equal(num_pred, 0);
     });
   });
-   describe("When owner creates a prediction and latest price feed was not updated beyong tolerance", async () => {
+   describe("When owner creates a prediction and latest price feed was not updated beyond tolerance", async () => {
     it("prediction is not saved", async () => {
-      var earlier = getUtcSeconds() - (1.5*TOL)
+      var diff = new BN(2).mul(TOL_BN);
+      var earlier = this.now.sub(diff);
       await this.priceFeedContract.setTimeStamp(earlier);
       await this.contract.addUser(this.other1);
       await expectRevert(
         this.contract.createPrediction(
           this.other1,
-          getUtcSeconds(),
+          this.now,
           "BTC",
           true, // LONG
           60*60*24*90, // 3 months
@@ -202,10 +204,9 @@ contract('PredictionManager', ([owner, other1, other2]) => {
  
   describe("When owner creates a prediction", async () => {
     it("prediction is saved with latest price feed", async () => {
-      var predTimeStamp = getUtcSeconds();
-      var now = getUtcSeconds()
+      var predTimeStamp = this.now.toString();
       await this.contract.addUser(this.other1);
-      await this.priceFeedContract.setTimeStamp(now);
+      await this.priceFeedContract.setTimeStamp(this.now);
       await this.contract.createPrediction(
         this.other1,
         predTimeStamp,
@@ -224,7 +225,7 @@ contract('PredictionManager', ([owner, other1, other2]) => {
       assert.equal(pred['symbol'], "BTC");
       assert.equal(pred['predDirection'], false);
       assert.equal(pred['initialPrice']['price'], BTC_PRICE);
-      assert.equal(pred['initialPrice']['timeStamp'], now);
+      assert.equal(pred['initialPrice']['timeStamp'], this.now.toString());
       assert.equal(pred['finalPrice']['price'], 0);
       assert.equal(pred['finalPrice']['timeStamp'], 0);
     });
